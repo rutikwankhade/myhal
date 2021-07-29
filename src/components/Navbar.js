d3ad14b202b8d557a63889354c9cef4e19d5b53a
rutik wankhade <rutikwankhade2@gmail.com>
2021-07-29 09:03:02 +0530

    add logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { UserButton, SignedIn, SignedOut } from '@clerk/clerk-react';
 import { Link } from 'react-router-dom'
+
+import yinyangIcon from '../assets/yinyang.png'
 const Navbar = () => {
 
     return (
         <div className="p-4 items-center md:px-24 flex flex-row bg-gray-50  border-gray-200 border-b-2">
-            <Link to="/dashboard" className="w-1/4 mr-auto ml-2">
-                <h1 className="font-inter font-extrabold text-3xl text-gray-600">myhal</h1>
+            <Link to="/dashboard" className="flex mx-2 items-center w-1/4 mr-auto ml-2">
+                <img src={yinyangIcon} alt="/" className="w-6 h-6 opacity-80"/>
+                <h1 className="font-inter font-extrabold text-3xl text-gray-600 mx-2">myhal</h1>
             </Link>
 
             <SignedIn>
