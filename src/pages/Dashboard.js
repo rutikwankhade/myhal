b8f494eecf1c469e43f33af37d3413c21f9beb78
rutik wankhade <rutikwankhade2@gmail.com>
2021-07-29 20:28:11 +0530

    added transitions to buttons

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -115,16 +115,17 @@ const Dashboard = () => {
                 reverseOrder={false}
             />
 
-            <div className="p-10 flex md:flex-row flex-col justify-center bg-purple-100 mx-auto">
+            <div className="md:px-10 flex md:flex-row flex-col justify-center bg-purple-100 mx-auto">
 
-                <div className="bg-white rounded-lg p-4 pr-8 md:w-1/3">
+                <div className="m-10 bg-white rounded-lg p-4 pr-8 md:w-1/3">
                     <div className="flex flex-row items-center justify-center">
-                        <Link to={`/profile/${username}`}>
-                            <h1 className="rounded-lg w-auto border-2 p-2 px-16 text-center text-2xl font-bold border-indigo-400 my-4 hover:bg-indigo-400 hover:text-white bg-white">Visit My Page</h1>
+                        
+                        <Link to={`/profile/${username}`} target="_blank" rel="noreferrer">
+                            <button className=" duration-200 ease-in-out rounded-lg w-auto border-2 p-2 px-16 text-center text-2xl font-bold border-indigo-400 my-4 hover:bg-indigo-400 hover:text-white bg-white">Visit My Page</button>
                         </Link>
 
                         <button onClick={copyLink}>
-                            <img src={copyIcon} alt="copy" className="w-12 h-12 mx-2 cursor-pointer rounded-xl border-2 p-2 hover:border-pink-300 border-indigo-400" />
+                            <img src={copyIcon} alt="copy" className="transform hover:scale-105 duration-300 ease-in-out w-12 h-12 mx-2 cursor-pointer rounded-xl border-2 p-2 hover:border-pink-300 border-indigo-400" />
                         </button>
                     </div>
 
@@ -133,7 +134,7 @@ const Dashboard = () => {
                     <textarea
                         defaultValue={data?.about}
                         className="w-full p-2 text-xl border-2 m-1 h-40 rounded-lg bg-gray-50 px-4"
-                        placeholder="Tell a little about yourself!"
+                        placeholder="// Tell a little about yourself!"
                         onChange={(e) => setBio(e.target.value)}
                     />
                     {/* <button
@@ -155,7 +156,7 @@ const Dashboard = () => {
 
                     <button
                         onClick={updateBio}
-                        className="text-xl bg-gray-700 p-2 my-2 m-1 rounded-xl text-white w-full text-center"
+                        className="transform hover:scale-105 hover:bg-gray-700 duration-300 ease-in-out text-xl bg-gray-600 p-2 my-2 m-1 rounded-xl text-white w-full text-center"
                     >
                         Update
                     </button>
@@ -164,7 +165,7 @@ const Dashboard = () => {
 
 
 
-                <div className="md:w-2/3 md:mx-20 flex flex-col">
+                <div className="md:w-2/3 md:m-10 flex flex-col">
 
 
                     <div className=" bg-white h-auto flex flex-col justify-center rounded-xl p-10  ">
@@ -192,7 +193,7 @@ const Dashboard = () => {
 
                         <button
                             onClick={addToProfile}
-                            className="text-xl bg-gray-700 p-2 my-2 m-1 rounded-xl text-white w-auto mx-auto px-6 text-center"
+                            className="transform hover:scale-105 hover:bg-gray-700 duration-300 ease-in-out text-xl bg-gray-600 p-2 my-2 m-1 rounded-xl text-white w-auto mx-auto px-6 text-center"
                         >
                             Add to my Profile
                         </button>
@@ -200,7 +201,7 @@ const Dashboard = () => {
 
 
 
-                    <div className="bg-white rounded-xl p-4 mt-2 flex  items-center justify-center">
+                    <div className="bg-white rounded-xl p-4 px-6 mt-2 flex  items-center justify-center">
                         <h1 className="text-lg text-gray-400 italic font-semibold ">
                             Let your friends know about your highs and lows.
                         </h1>
