99e7cb79cfc67c744bfbf8fb7eb8b08620bc7895
rutik wankhade <rutikwankhade2@gmail.com>
2021-07-29 18:15:19 +0530

    updated hero section with 3d illustrations

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ import yinyangIcon from '../assets/yinyang.png'
 const Navbar = () => {
 
     return (
-        <div className="p-4 items-center md:px-24 flex flex-row bg-gray-50  border-gray-200 border-b-2">
+        <div className="p-4 items-center md:px-24 flex flex-row bg-white  border-gray-200 border-b-2">
             <Link to="/dashboard" className="flex mx-2 items-center w-1/4 mr-auto ml-2">
                 <img src={yinyangIcon} alt="/" className="w-6 h-6 opacity-80"/>
                 <h1 className="font-inter font-extrabold text-3xl text-gray-600 mx-2">myhal</h1>
@@ -17,7 +17,7 @@ const Navbar = () => {
             </SignedIn>
             <SignedOut>
                 <Link to="/signin">
-                    <button className="w-32 bg-pink-400 hover:bg-pink-500 border-white border-2 text-white rounded-lg p-2 px-6 text-xl font-semibold">Sign in</button>
+                    <button className="w-32 bg-pink-400 hover:bg-pink-500 border-white border-2 text-white rounded-lg p-2 px-6 text-xl font-semibold transform hover:scale-105 duration-300 ease-in-out">Sign in</button>
                 </Link>
             </SignedOut>
         </div>
--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import readingImg from '../assets/reading.png'
+import readingImg from '../assets/hero1.png'
 import {Link} from 'react-router-dom'
 
 const Welcome = () => {
     return (
-        <div className=" ">
-            <div className="p-20 mx-10 flex flex-row">
-                <div>
-                    <h1 className="leading-loose mx-4 my-8  text-4xl font-bold font-inter italic text-gray-600  ">Celebrate your small <span className="text-indigo-400">wins</span><br /> and embrace <span className="text-indigo-400">failures</span>.</h1>
+        <div className=" bg-purple-100">
+            <div className="p-4   flex flex-row justify-center">
+                <div className="mt-20 px-4 ml-10">
+                    <h1 className="leading-loose mx-4 my-8  text-4xl font-bold font-inter text-gray-600  ">Celebrate your small <span className="text-indigo-400 italic">wins</span><br /> and embrace <span className="italic text-indigo-400">failures</span>.</h1>
                  <Link to="/signup">
-                    <button className=" bg-gray-700 hover:bg-indigo-500 border-white border-2 text-white   rounded-lg p-2 px-6 text-xl  ml-4 font-bold">Get Started</button>
+                    <button className=" bg-gray-700 hover:bg-indigo-500 border-white border-2 text-white transform hover:scale-105 duration-300 ease-in-out rounded-lg p-2 px-6 text-2xl  ml-4 font-semibold">Get Started</button>
                 </Link>
                 </div>
 
-                <img src={readingImg} alt="illustration" className=" w-1/2 ml-auto mr-2" />
+                <img src={readingImg} alt="illustration" className="w-6/12 " />
             </div>
 
             <div className="bg-gray-50 p-40">jj</div>
