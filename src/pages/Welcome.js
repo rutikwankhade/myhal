66e8a8ffa9538c6d0a1401e222d5d4125d2d0f16
rutik wankhade <rutikwankhade2@gmail.com>
2021-07-30 00:51:28 +0530

    add animations

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -6,21 +6,32 @@ import demoImg from '../assets/demo.png'
 
 import { Link } from 'react-router-dom'
 
+import Fade from 'react-reveal/Fade';
+
+
 const Welcome = () => {
     return (
         <div className=" bg-purple-100">
-            <div className="p-4 flex md:flex-row flex-col justify-center">
-                <div className="md:mt-28 px-4 md:ml-10">
-                    <h1 className="leading-loose mx-4 my-8  md:text-4xl text-xl font-bold font-inter text-gray-600  ">Celebrate your small <span className="text-indigo-400">wins</span><br /> and embrace <span className=" text-indigo-400">failures</span>.</h1>
-                    <Link to="/signup">
-                        <button className=" bg-gray-700 hover:bg-indigo-500 border-white border-2 text-white transform hover:scale-105 duration-300 ease-in-out rounded-lg p-2 px-6 md:text-2xl  ml-4 font-semibold">Get Started</button>
-                    </Link>
+            <Fade bottom>
+
+                <div className="p-4 flex md:flex-row flex-col justify-center">
+
+                    <div className="md:mt-28 px-4 md:ml-10">
+                        <h1 className="leading-loose mx-4 my-8  md:text-4xl text-xl font-bold font-inter text-gray-600  ">Celebrate your small <span className="text-indigo-400">wins</span><br /> and embrace <span className=" text-indigo-400">failures</span>.</h1>
+                        <Link to="/signup">
+                            <button className=" bg-gray-700 hover:bg-indigo-500 border-white border-2 text-white transform hover:scale-105 duration-300 ease-in-out rounded-lg p-2 px-6 md:text-2xl  ml-4 font-semibold">Get Started</button>
+                        </Link>
+                    </div>
+
+
+                    <img src={readingImg} alt="illustration" className="md:w-6/12 " />
                 </div>
+            </Fade>
 
-                <img src={readingImg} alt="illustration" className="md:w-6/12 " />
-            </div>
+
+
+
             <img src={demoImg} alt="illustration" className="w-10/12 mx-auto transform md:-translate-y-60 " />
-
 
 
 
@@ -28,31 +39,35 @@ const Welcome = () => {
                 <img src={hero2} alt="illustration" className="md:w-6/12 " />
 
                 <div className="font-inter  md:w-1/2 p-10 mx-auto transform md:-translate-y-40">
+                    <Fade right>
 
-                    <h1 className="md:text-4xl text-3xl py-4 font-bold text-gray-700">How does it work?</h1>
+                        <h1 className="md:text-4xl text-3xl py-4 font-bold text-gray-700">How does it work?</h1>
 
-                    <div className="flex my-2">
-                        <div className="transform hover:scale-105 duration-300 ease-in-out w-20 h-20 cursor-pointer flex flex-col justify-center items-center  m-2 bg-white rounded-xl border-2 border-gray-600 p-4">
-                            <span className="text-4xl">✨</span>
+                        <div className="flex my-2">
+                            <div className="transform hover:scale-105 duration-300 ease-in-out w-20 h-20 cursor-pointer flex flex-col justify-center items-center  m-2 bg-white rounded-xl border-2 border-gray-600 p-4">
+                                <span className="text-4xl">✨</span>
+                            </div>
+
+                            <p className="md:w-1/2 m-2 text-xl text-gray-700 font-semibold"><span className="text-indigo-400 font-bold">Highlights </span>is the list of small wins that you are proud of throughout your journey. Celebrate them. </p>
                         </div>
 
-                        <p className="md:w-1/2 m-2 text-xl text-gray-700 font-semibold"><span className="text-indigo-400 font-bold">Highlights </span>is the list of small wins that you are proud of throughout your journey. Celebrate them. </p>
-                    </div>
+                        <div className="flex my-2">
+                            <div className="transform hover:scale-105 duration-300 ease-in-out w-20 h-20 cursor-pointer flex flex-col justify-center items-center  m-2 bg-white rounded-xl border-2 border-gray-600 p-4">
+                                <span className="text-4xl">🌊</span>
+                            </div>
 
-                    <div className="flex my-2">
-                        <div className="transform hover:scale-105 duration-300 ease-in-out w-20 h-20 cursor-pointer flex flex-col justify-center items-center  m-2 bg-white rounded-xl border-2 border-gray-600 p-4">
-                            <span className="text-4xl">🌊</span>
+                            <p className="md:w-1/2 m-2 text-xl text-gray-700 font-semibold"><span className="text-pink-400 font-bold">Lowlights </span>is the list of moments when you failed, learned and grew up. Embrace them.</p>
                         </div>
+                    </Fade>
 
-                        <p className="md:w-1/2 m-2 text-xl text-gray-700 font-semibold"><span className="text-pink-400 font-bold">Lowlights </span>is the list of moments when you failed, learned and grew up. Embrace them.</p>
-                    </div>
+                    <Fade right>
 
-
-                    <h1 className="text-3xl mt-20 py-4 font-bold text-gray-700">Why create one?</h1>
-                    <p className="md:w-2/3 m-2 text-2xl text-gray-700 font-semibold">Documenting and sharing your highs and lows will work as a motivation to keep going. And help you see your progress.</p>
-                    <Link to="/signup">
-                        <button className=" bg-indigo-400 hover:bg-indigo-500 border-white border-2 text-white transform hover:scale-105 duration-300 ease-in-out rounded-lg p-2 px-6 text-2xl  my-4 font-semibold">Let's do this.</button>
-                    </Link>
+                        <h1 className="text-3xl mt-20 py-4 font-bold text-gray-700">Why create one?</h1>
+                        <p className="md:w-2/3 m-2 text-2xl text-gray-700 font-semibold">Documenting and sharing your highs and lows will work as a motivation to keep going. And help you see your progress.</p>
+                        <Link to="/signup">
+                            <button className=" bg-indigo-400 hover:bg-indigo-500 border-white border-2 text-white transform hover:scale-105 duration-300 ease-in-out rounded-lg p-2 px-6 text-2xl  my-4 font-semibold">Let's do this.</button>
+                        </Link>
+                    </Fade>
 
                 </div>
             </div>
